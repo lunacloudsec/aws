0ed34bb1886203d6a9eabcec2637bc5dd2d38b11
Luna Cloud Security <92065894+lunacloudsec@users.noreply.github.com>
2021-12-03 09:57:36 -0300

    Update creating_snapshot.py

--- creating_snapshot.py
+++ creating_snapshot.py
@@ -10,7 +10,7 @@ Percorre todos os volumes atachados nas instancias EC2 (da conta autenticada) e 
 for vol in ec2.volumes.all():
     vol_id = vol.id # Variavel do volume id de cada instancia
     volume = ec2.Volume(vol_id)
-    desc = 'Snapshot do Volume {} - Patch Manager {}'.format(vol_id,hoje) # Descrição do snapshot
+    desc = 'Snapshot do Volume {} - Data {}'.format(vol_id,hoje) # Descrição do snapshot
     print("Criando snapshot do Volume: ", vol_id)
     volume.create_snapshot(Description=desc,
         TagSpecifications=[
@@ -20,7 +20,7 @@ for vol in ec2.volumes.all():
             'Tags': [
                 {
                     'Key': 'Name',
-                    'Value': 'snapshot-poc-18-11-2021'
+                    'Value': 'my-snapshot-poc'
                 },
             ]
         },
